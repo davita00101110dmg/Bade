eed1a3423b92d162349e763ae44af0a0303c2282
Daviti Khvedelidze <davitikhvedelidze26@gmail.com>
2026-08-09 10:12:28 +0400

    Initial Commit

---
+++ Bade/BadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BadeApp.swift
+//  Bade
+//
+//  Created by Daviti Khvedelidze on 8/9/26.
+//
+
+import SwiftUI
+
+@main
+struct BadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BadeTests/BadeTests.swift
@@ -0,0 +1,18 @@
+//
+//  BadeTests.swift
+//  BadeTests
+//
+//  Created by Daviti Khvedelidze on 8/9/26.
+//
+
+import Testing
+
+struct BadeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
